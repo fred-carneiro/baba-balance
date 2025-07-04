16abe7e07e501c8f589ae8adfa33cbbdca917228
Frederico Carneiro <frederico.silva@tst.jus.br>
2025-07-04 20:44:00 -0300

    Fix Zustand store TypeScript errors for deployment

--- src/lib/store/jogadores.ts
+++ src/lib/store/jogadores.ts
@@ -13,13 +13,13 @@ interface JogadoresState {
 
 export const useJogadoresStore = create<JogadoresState>()(
   persist(
-    (set: (fn: (state: JogadoresState) => Partial<JogadoresState> | JogadoresState) => void, get: () => JogadoresState) => ({
+    (set) => ({
       jogadores: [],
       participantes: [],
-      addJogador: (jogador: Jogador) => set({ jogadores: [...get().jogadores, jogador] }),
-      updateJogador: (jogador: Jogador) => set({ jogadores: get().jogadores.map((j: Jogador) => j.id === jogador.id ? jogador : j) }),
-      removeJogador: (id: string) => set({ jogadores: get().jogadores.filter((j: Jogador) => j.id !== id) }),
-      setParticipantes: (ids: string[]) => set({ participantes: ids }),
+      addJogador: (jogador: Jogador) => set((state) => ({ jogadores: [...state.jogadores, jogador] })),
+      updateJogador: (jogador: Jogador) => set((state) => ({ jogadores: state.jogadores.map((j: Jogador) => j.id === jogador.id ? jogador : j) })),
+      removeJogador: (id: string) => set((state) => ({ jogadores: state.jogadores.filter((j: Jogador) => j.id !== id) })),
+      setParticipantes: (ids: string[]) => set(() => ({ participantes: ids })),
     }),
     {
       name: 'jogadores-storage',
